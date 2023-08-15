24f2654712845f6787f54cee75830d0587544d3d
Sepehr <bazyar.sepehr@gmail.com>
2023-08-15 19:00:32 +0330

    fix: rename is_active field to is_deleted name

--- core/manager.py
+++ core/manager.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class CustomQuerySet(models.QuerySet):
     def delete(self):
-        return self.update(is_active=False)
+        return self.update(is_deleted=False)
 
 
 class CustomManager(models.Manager):
     def get_queryset(self):
-        return CustomQuerySet(self.model, self._db).filter(is_active=True)
+        return CustomQuerySet(self.model, self._db).filter(is_deleted=True)
