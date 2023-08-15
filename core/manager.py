77d6385ceb1a6ebb8f523307e53bc40e9f35d047
Sepehr <bazyar.sepehr@gmail.com>
2023-08-15 20:12:38 +0330

    fix: replacement boolean false true soft delete

--- core/manager.py
+++ core/manager.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class CustomQuerySet(models.QuerySet):
     def delete(self):
-        return self.update(is_deleted=False)
+        return self.update(is_deleted=True)
 
 
 class CustomManager(models.Manager):
     def get_queryset(self):
-        return CustomQuerySet(self.model, self._db).filter(is_deleted=True)
+        return CustomQuerySet(self.model, self._db).filter(is_deleted=False)
