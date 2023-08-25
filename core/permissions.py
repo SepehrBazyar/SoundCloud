146a5ad283da78dcd1dee5e871461fd0d3de0948
Sepehr <bazyar.sepehr@gmail.com>
2023-08-25 11:59:17 +0330

    style: reformatted the custom permission module

--- core/permissions.py
+++ core/permissions.py
@@ -3,6 +3,7 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnlyPermission(permissions.BasePermission):
     message = "Owner or read only permission of album"
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
